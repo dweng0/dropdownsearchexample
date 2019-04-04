efd0f2d8955906919319d55cfef72ab6d7351d3d
Jay <jmartinsmith@outlook.com>
2019-04-04 20:30:12 +0000

    in
    init commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CountryList from './components/CountryList';
+import Countries from './Constants/Countries'
 
 const App = () => {
     return(
         <div className="ui one column stackable center aligned page grid">
         <div className="column twelve wide">
-            <CountryList/>
+            <CountryList countries={Countries}/>
         </div>
      </div>
     );
